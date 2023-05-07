81d8567dbe90d58030241fc892e4165748994b9f
AKSUF <abu053125@gmail.com>
2023-05-07 12:45:05 +0600

    how to use useState in react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div className="App bg-red-900">
+<Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
